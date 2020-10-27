3bbb88784b48fe20a1aa6675c813378bac455ae8
cristianolaya <warrdnez@gmail.com>
2020-10-26 22:02:38 -0500

    adding sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,27 @@ var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 var connect = require('gulp-connect-php');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', done => {
+  gulp.src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle : 'compressed'}))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+  done();
+});
+
+// end config sass
+
 
 gulp.task('watch', done => {
+  gulp.watch('./scss/**/*.scss').on('change', gulp.series('sass'));
   gulp.watch('./js/**/*.js').on('change', browserSync.reload);
   gulp.watch('./css/**/*.css').on('change', browserSync.reload);
   gulp.watch('./templates/**/*.html.twig').on('change', browserSync.reload);
